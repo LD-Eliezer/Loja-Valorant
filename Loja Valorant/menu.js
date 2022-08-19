839bb9dd57a414381ecc3b3d0ab1da140f04eb12
LD-Eliezer <104364270+LD-Eliezer@users.noreply.github.com>
2022-08-18 23:07:58 -0300

    Add files via upload

--- Loja Valorant/menu.js
+++ Loja Valorant/menu.js
@@ -11,6 +11,41 @@ function scrollFunction() {
 }
 
 function baixo() {
-    document.body.scrollTop = 945;
-    document.documentElement.scrollTop = 945;
+    document.body.scrollTop = 800;
+    document.documentElement.scrollTop = 800;
+}
+
+var bot2 = document.getElementById('cm')
+
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      bot2.style.display = "block";
+    } else {
+      bot2.style.display = "none";
+    }
+}
+
+function cima() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//BOTAO DE CONTINUAR
+var bot3 = document.getElementById('ct')
+
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      bot3.style.display = "block";
+    } else {
+      bot3.style.display = "none";
+    }
+}
+
+function continuar() {
+    document.body.scrollTop = 2000;
+    document.documentElement.scrollTop = 2000;
 }
